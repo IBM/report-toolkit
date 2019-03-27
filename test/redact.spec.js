50ed526b6167a65dd1cdcd21942f948c1520cac4
Christopher Hiller <christopher.hiller@ibm.com>
2019-03-27 10:53:53 -0700

    only redact environment variables; update tests

--- test/redact.spec.js
+++ test/redact.spec.js
@@ -10,26 +10,26 @@ describe('module:redact', function() {
           FOO_SESSION_ID: 'aklsdjhflskfjdf',
           PASSWD: 'alsjkdfkl',
           OK: 'ok',
-          stuff: {
+          environmentVariables: {
             SECRET: 'kjdfgshfkdsjfh',
             API_KEY: 'kldjfghn,vcx',
-            'ACCESS-KEY': 'gkasdjhfksd'
-          },
-          things: ['THING_TOKEN']
+            'ACCESS-KEY': 'gkasdjhfksd',
+            things: ['THING_TOKEN']
+          }
         };
       });
 
-      it('should redact the values', function() {
+      it('should redact the values in the "interesting" props', function() {
         expect(redact(obj), 'to equal', {
-          FOO_SESSION_ID: REDACTED,
-          PASSWD: REDACTED,
+          FOO_SESSION_ID: 'aklsdjhflskfjdf',
+          PASSWD: 'alsjkdfkl',
           OK: 'ok',
-          stuff: {
+          environmentVariables: {
             SECRET: REDACTED,
             API_KEY: REDACTED,
-            'ACCESS-KEY': REDACTED
-          },
-          things: REDACTED
+            'ACCESS-KEY': REDACTED,
+            things: REDACTED
+          }
         });
       });
     });
