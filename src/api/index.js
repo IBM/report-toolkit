5bfa556fab5fb66b32a64fd3a4534461e06434cf
Christopher Hiller <Christopher.Hiller@ibm.com>
2019-05-08 10:10:47 -0700

    move promise API into its own module but remain the default

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,5 @@
 import * as observable from './observable';
 
-import {createDebugger} from '../debug';
-import {toArray} from 'rxjs/operators';
-
-export const debug = createDebugger(module);
-
-export const inspect = async (...args) =>
-  observable
-    .inspect(...args)
-    .pipe(toArray())
-    .toPromise();
-
-export const loadConfig = async (...args) =>
-  observable.loadConfig(...args).toPromise();
-
-export const loadRules = async (...args) =>
-  observable
-    .loadRules(...args)
-    .pipe(toArray())
-    .toPromise();
-
-export const diff = async (...args) =>
-  observable
-    .diff(...args)
-    .pipe(toArray())
-    .toPromise();
-
-export const readReports = async (...args) =>
-  observable.readReports(...args).toPromise();
+export * from './promise';
 
 export {observable};
