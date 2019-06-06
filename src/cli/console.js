95bf5c24534bc52f6c656855c7eff93ee6d67c63
Christopher Hiller <Christopher.Hiller@ibm.com>
2019-06-06 12:43:22 -0700

    use log-symbols module

--- src/cli/console.js
+++ src/cli/console.js
@@ -1,5 +1,6 @@
 import * as formatters from '../formatters';
 
+import {error, success} from 'log-symbols';
 import {map, pipeIf} from '../observable';
 
 import _ from 'lodash';
@@ -7,8 +8,9 @@ import colors from './colors';
 import stripAnsi from 'strip-ansi';
 import termsize from 'term-size';
 
-export const ok = text => colors.green('✓') + ' ' + colors.green().bold(text);
-export const fail = text => colors.red('✕') + ' ' + colors.red().bold(text);
+export const ok = text =>
+  colors.green(success) + ' ' + colors.green().bold(text);
+export const fail = text => colors.red(error) + ' ' + colors.red().bold(text);
 
 export const toFormattedString = (format, opts = {}) => observable =>
   observable.pipe(
