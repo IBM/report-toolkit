IBM/report-toolkit

commit cab080aa9377d4e86e3076a627f413eb18f4499e

doc(common): fix some typings... yuck

packages/common/src/observable.js
@@ -81,8 +81,8 @@ export const pipeIf = (predicate, operator, ...operators) => {
 /**
  * Essentially wraps `_.orderBy()`
  * @see https://lodash.com/docs/4.17.11#orderBy
- * @param {string|string[]|Function|Function[]|Object|Object[]} iteratee - Any supported LoDash iteratee or list thereof
- * @param {string|string[]} [direction=asc] - Order in which to sort (`asc` or `desc`) or list thereof, corresponding to each item in `iteratee` (if `iteratee` is a list)
+ * @param {import('lodash').List<any>} iteratee - Any supported LoDash iteratee or list thereof
+ * @param {import('lodash').Many<boolean|"asc"|"desc">} [direction=asc] - Order in which to sort (`asc` or `desc`) or list thereof, corresponding to each item in `iteratee` (if `iteratee` is a list)
  * @returns {import('rxjs').OperatorFunction<any,any>}
  */
 export const sort = (iteratee = _.identity, direction = 'asc') => observable =>

commit 7301ed58a22a9ebe2053a3b911ba82526dccc29a

fix(common): fix bug introduced by cab080aa9377d4e86e3076a627f413eb18f4499e

packages/cli/src/commands/transform.js
@@ -1,18 +1,20 @@
 import {_, createDebugPipe, error, observable} from '@report-toolkit/common';
 import {FORMAT_CSV, FORMAT_JSON, FORMAT_PIPE} from '@report-toolkit/formatters';
-import {toObjectFromFilepath} from '@report-toolkit/fs';
-import {constants, transformers} from '@report-toolkit/transformers';
+import {
+  constants as transformerNames,
+  transformers
+} from '@report-toolkit/transformers';
 import {writeFileSync} from 'fs';
 
 import {colors, toFormattedString} from '../console-utils.js';
 import {FORMAT_TABLE} from '../table-formatter.js';
-import {GROUPS, OPTIONS} from './common.js';
+import {fromFilepathToReport, GROUPS, OPTIONS} from './common.js';
 
-const {fromAny, iif, throwRTkError} = observable;
+const {iif, throwRTkError} = observable;
+
 const {REPORT_TOOLKIT_ERR_INVALID_CLI_OPTION} = error;
 
 const debug = createDebugPipe('cli', 'commands', 'transform');
-const {TRANSFORMER_NUMERIC} = constants;
 const ALLOWED_FORMATS = [FORMAT_CSV, FORMAT_JSON, FORMAT_TABLE, FORMAT_PIPE];
 
 export const command = 'transform <transformer> <file..>';
@@ -43,7 +45,7 @@ export const builder = yargs =>
       type: 'array'
     })
     .positional('transform', {
-      choices: [TRANSFORMER_NUMERIC]
+      choices: Object.keys(transformerNames)
     });
 
 export const handler = argv => {
@@ -51,7 +53,6 @@ export const handler = argv => {
     file: filepaths,
     transformer,
     config,
-    field,
     truncate: truncateValues = true,
     wrap: wrapValues = false,
     format = FORMAT_JSON,
@@ -63,10 +64,15 @@ export const handler = argv => {
   const transform = transformers[transformer];
   iif(
     () => transform,
-    fromAny(filepaths).pipe(
-      toObjectFromFilepath({...config.transform, showSecretsUnsafe}),
-      debug(transformer => `using transformer "${transformer}"`),
-      transform({...config.transform, field}),
+    fromFilepathToReport(filepaths, {
+      ...config.transform,
+      showSecretsUnsafe
+    }).pipe(
+      debug(() => `using transformer "${transformer}"`),
+      transform({
+        ...config.transform,
+        ..._.getOr({}, `transform.${transformer}`, config)
+      }),
       toFormattedString(format, {
         color,
         fields: [

packages/common/src/observable.js
@@ -43,38 +43,22 @@ import {
   toArray
 } from 'rxjs/operators/index.js';
 
-import {RTkError} from './error.js';
+import {createRTkError} from './error.js';
 import {_} from './util.js';
 
 /**
  * Pipes source Observable to one or more Operators if the predicate is truthy.
  * If falsy, just returns the source Observable.
  * @param {Function|any} predicate - If a function, evaluated with the value from the source Observable. Anything else is evaluated when called.
- * @param {import('rxjs').OperatorFunction<any,any>} operator - One or more RxJS operators to pipe to
  * @param {...import('rxjs').OperatorFunction<any,any>} operators - More RxJS operators to pipe to
  * @returns {import('rxjs').OperatorFunction<any,any>}
  */
-export const pipeIf = (predicate, operator, ...operators) => {
+export const pipeIf = (predicate, ...operators) => {
   predicate = _.isFunction(predicate) ? predicate : _.constant(predicate);
   return observable =>
     observable.pipe(
-      mergeMap(v =>
-        predicate(v)
-          ? of(v).pipe(
-              // this can't be how it is, right?
-              operator,
-              operators[0],
-              operators[1],
-              operators[2],
-              operators[3],
-              operators[4],
-              operators[5],
-              operators[6],
-              operators[7],
-              operators[8]
-            )
-          : of(v)
-      )
+      // @ts-ignore
+      mergeMap(v => (predicate(v) ? of(v).pipe(...operators) : of(v)))
     );
 };
 
@@ -118,7 +102,7 @@ export const fromAny = value =>
  * @returns {import('rxjs').Observable} An Observable emitting a single error
  */
 export const throwRTkError = (code, message, opts = {}) =>
-  throwError(RTkError.create(code, message, opts));
+  throwError(createRTkError(code, message, opts));
 
 /**
  * A simple operator that parses a JSON string into the resulting JS representation.