5e6fd76cc1bd4e5c846f8de56dbd08ae413a35e1
Christopher Hiller <boneskull@boneskull.com>
2019-10-11 16:14:29 -0700

    chore: add syncpack; move lint-staged cfg to config file
    syncpack does some package.json formatting and also has some other handy checks

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  '*.js': ['eslint --fix', 'git add'],
+  '*.{yml,md,mdx}': ['prettier --write', 'git add'],
+  'packages/*/package.json':
+    /**
+     * syncpack works against all package manifests, so this will be one command
+     * and then a `git add` for each file
+     * @param {string[]} filepaths
+     */
+    filepaths => [
+      'syncpack',
+      ...filepaths.map(filepath => `git add "${filepath}"`)
+    ]
+};
