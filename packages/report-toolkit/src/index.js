77f8c1f2287ec8b4d4cbfefe446305592f6e7fde
Christopher Hiller <boneskull@boneskull.com>
2019-10-17 10:34:26 -0400

    fix: missing shebang

--- packages/report-toolkit/src/index.js
+++ packages/report-toolkit/src/index.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
+
 import {main} from '@report-toolkit/cli';
+import * as core from '@report-toolkit/core';
 
 if (require.main === module) {
   main();
 }
 
-export * from '@report-toolkit/core';
+export default core;
