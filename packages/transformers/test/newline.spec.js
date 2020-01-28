0ef74aecfb7534fcbde44fa7dc9e77edb84c0639
Christopher Hiller <boneskull@boneskull.com>
2020-01-28 15:26:52 -0800

    fix(transformers): fix bad newline unit test

--- packages/transformers/test/newline.spec.js
+++ packages/transformers/test/newline.spec.js
@@ -3,11 +3,12 @@ import {of} from '@report-toolkit/common/src/observable.js';
 import {transform} from '../src/newline.js';
 
 describe('@report-toolkit/transformers:newline', function() {
+  // note that the actual newline characters are added by the CLI via console.log!
+
   it('should parse a stream of JS objects into newline-delimited JSON', function() {
     return expect(
       of({bar: 2, baz: 3, foo: 1}, {bar: 5, baz: 6, foo: 4}).pipe(
         transform({
-          newline: '\n',
           fields: [
             {label: 'Foo', value: 'foo'},
             {label: 'Bar', value: 'bar'},
@@ -16,26 +17,28 @@ describe('@report-toolkit/transformers:newline', function() {
         })
       ),
       'to complete with values',
-      '{"bar":2,"baz":3,"foo":1}\n',
-      '{"bar":5,"baz":6,"foo":4}\n'
+      '{"bar":2,"baz":3,"foo":1}',
+      '{"bar":5,"baz":6,"foo":4}'
     );
   });
 
-  it('should parse a stream of strings into newline-delimited strings', function() {
-    return expect(
-      of('foo', 'bar').pipe(
-        transform({
-          newline: '\n',
-          fields: [
-            {label: 'Foo', value: 'foo'},
-            {label: 'Bar', value: 'bar'},
-            {label: 'Baz', value: 'baz'}
-          ]
-        })
-      ),
-      'to complete with values',
-      'foo\n',
-      'bar\n'
-    );
+  describe('when the "json" option is "false"', function() {
+    it('should parse a stream of strings into newline-delimited strings', function() {
+      return expect(
+        of('foo', 'bar').pipe(
+          transform({
+            fields: [
+              {label: 'Foo', value: 'foo'},
+              {label: 'Bar', value: 'bar'},
+              {label: 'Baz', value: 'baz'}
+            ],
+            json: false
+          })
+        ),
+        'to complete with values',
+        'foo',
+        'bar'
+      );
+    });
   });
 });
