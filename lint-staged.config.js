b7ffc47fb0831971672fc9f81b7765305a5cbc27
Christopher Hiller <boneskull@boneskull.com>
2020-01-31 14:47:08 -0800

    chore: fix lockfile in docs
    - format TOML with Prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   '*.js': ['eslint --fix'],
-  '*.{yml,md,mdx}': ['prettier --write'],
+  '*.{yml,md,mdx,toml}': ['prettier --write'],
   'packages/*/package.json': ['syncpack format'],
   './README.md': [
     'sync-monorepo-packages --no-package-json -p packages/report-toolkit --force README.md'
