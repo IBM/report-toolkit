0005839644d69effb7708f656b18cd7a2dfdac41
Christopher Hiller <boneskull@boneskull.com>
2020-02-03 14:23:31 -0800

    chore: fix sync of README.md
    update lint-staged config to explicitly add `packages/report-toolkit/README.md` when the root one is updated

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,7 @@ module.exports = {
   '*.{yml,md,mdx,toml}': ['prettier --write'],
   'packages/*/package.json': ['syncpack format'],
   './README.md': [
-    'sync-monorepo-packages --no-package-json -p packages/report-toolkit --force README.md'
+    'sync-monorepo-packages --no-package-json -p packages/report-toolkit --force README.md',
+    'git add packages/report-toolkit/README.md'
   ]
 };
