350d5f892cca5986b555775c39cccbbf7dba0574
Christopher Hiller <boneskull@boneskull.com>
2020-02-07 11:36:04 -0800

    docs: fix borked breadcrumbs

--- packages/docs/src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
+++ packages/docs/src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
@@ -1,5 +1,5 @@
 import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
-
+import {Breadcrumb, BreadcrumbItem} from 'carbon-components-react';
 import {EmbedCode, InlineCode, Metadata} from '../../../components';
 import Code from '../Code';
 import H4 from '../markdown/H4';
@@ -10,5 +10,7 @@ export default {
   code: Code,
   inlineCode: InlineCode,
   EmbedCode,
-  Metadata
+  Metadata,
+  Breadcrumb,
+  BreadcrumbItem
 };
