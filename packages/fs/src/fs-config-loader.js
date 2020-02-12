b11a3290296d16c51523d3cad13ebde8f353172f
Christopher Hiller <boneskull@boneskull.com>
2020-02-12 13:32:41 -0800

    feat(fs): search for config in usual places; closes #83
    This will additionally search for an `.rtkrc.js` or `rtk.config.js` in
    `$HOME`, `$(npm prefix)/etc`, and if on POSIX OS, XDG config dir(s) and
    `/etc`, in that order.
    
    I note that it's not particularly ergonomic to do this using
    `cosmiconfig`'s API.

--- packages/fs/src/fs-config-loader.js
+++ packages/fs/src/fs-config-loader.js
@@ -1,5 +1,9 @@
 import {_, createDebugPipe, error, observable} from '@report-toolkit/common';
+import path from 'path';
 import cosmiconfig from 'cosmiconfig';
+import xdgBasedir from 'xdg-basedir';
+import globalDirs from 'global-dirs';
+import os from 'os';
 
 import {RC_NAMESPACE} from './constants.js';
 
@@ -8,13 +12,25 @@ const {
   map,
   mapTo,
   mergeMap,
+  filter,
+  concatMap,
+  concatMapTo,
   of,
   pipeIf,
   switchMapTo,
+  take,
   throwRTkError
 } = observable;
 
 const debug = createDebugPipe('cli', 'loaders', 'config');
+
+const EXTRA_SEARCH_DIRS = [
+  os.homedir(),
+  path.join(globalDirs.npm.prefix, 'etc')
+];
+if (os.platform() !== 'win32') {
+  EXTRA_SEARCH_DIRS.push(...xdgBasedir.configDirs, '/etc');
+}
 
 const getExplorer = _.memoize(opts =>
   cosmiconfig(
@@ -29,7 +45,12 @@ const getExplorer = _.memoize(opts =>
   )
 );
 
-const toConfigFromSearchPath = (opts = {}) => {
+/**
+ *
+ * @param {cosmiconfig.ExplorerOptions} [opts] - Extra opts for cosmiconfig
+ * @returns {import('rxjs').OperatorFunction<string,object>}
+ */
+function toConfigFromSearchPath(opts = {}) {
   const explorer = getExplorer(opts);
   return observable =>
     observable.pipe(
@@ -40,34 +61,46 @@ const toConfigFromSearchPath = (opts = {}) => {
         map(_.get('config.config'))
       )
     );
-};
+}
 
 /**
  *
  * @param {cosmiconfig.ExplorerOptions} [opts] - Extra opts for cosmiconfig
  * @returns {import('rxjs').OperatorFunction<string,object>}
  */
-const toConfigFromFilepath = (opts = {}) => {
+function toConfigFromFilepath(opts = {}) {
   const explorer = getExplorer(opts);
   return observable =>
     observable.pipe(
       mergeMap(filepath => explorer.load(filepath)),
       map(_.get('config.config'))
     );
-};
+}
 
 export const fromFilesystemToConfig = ({
   config: rawConfigOrFilepath = {},
   searchPath = process.cwd(),
   search = true
-} = {}) =>
-  of(rawConfigOrFilepath).pipe(
+} = {}) => {
+  const allSearchPaths = [searchPath, ...EXTRA_SEARCH_DIRS];
+  return of(rawConfigOrFilepath).pipe(
     pipeIf(_.isString, toConfigFromFilepath()),
     pipeIf(
-      rawConfig => _.isEmpty(rawConfig) && search,
-      debug(() => `searching in ${searchPath} for config`),
-      mapTo(searchPath),
-      toConfigFromSearchPath()
+      /** @param {object} rawConfig */ rawConfig =>
+        _.isEmpty(rawConfig) && search,
+      concatMapTo(allSearchPaths),
+      debug(allSearchPath => `searching in ${allSearchPath} for config`),
+      concatMap(allSearchPath =>
+        of(allSearchPath).pipe(
+          pipeIf(allSearchPath === searchPath, toConfigFromSearchPath()),
+          pipeIf(
+            allSearchPath !== searchPath,
+            toConfigFromSearchPath({stopDir: allSearchPath})
+          )
+        )
+      ),
+      filter(_.negate(_.isEmpty)),
+      take(1)
     ),
     pipeIf(
       _.isEmpty && _.isString(rawConfigOrFilepath),
@@ -80,3 +113,4 @@ export const fromFilesystemToConfig = ({
     ),
     pipeIf(_.isEmpty, mapTo({}))
   );
+};
