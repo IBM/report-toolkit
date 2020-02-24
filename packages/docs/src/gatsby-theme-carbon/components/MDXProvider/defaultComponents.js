3499dbd051dbb5c12cffab8aeb434ff4100f5d76
Christopher Hiller <boneskull@boneskull.com>
2020-02-24 15:35:57 -0800

    chore(docs): remove EmbedCode; use gatsby-remark-import-code

--- packages/docs/src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
+++ packages/docs/src/gatsby-theme-carbon/components/MDXProvider/defaultComponents.js
@@ -1,6 +1,6 @@
 import defaultComponents from 'gatsby-theme-carbon/src/components/MDXProvider/defaultComponents';
 import {Breadcrumb, BreadcrumbItem} from 'carbon-components-react';
-import {EmbedCode, InlineCode, Metadata} from '../../../components';
+import {InlineCode, Metadata} from '../../../components';
 import Code from '../Code';
 import H4 from '../markdown/H4';
 
@@ -9,7 +9,6 @@ export default {
   h4: H4,
   code: Code,
   inlineCode: InlineCode,
-  EmbedCode,
   Metadata,
   Breadcrumb,
   BreadcrumbItem
